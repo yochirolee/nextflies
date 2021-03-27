ffde794c57bf518f9583aa58cd97f090e3f00e3c
unknown <yleecruz@gmail.com>
2021-03-27 11:35:55 -0400

    some performance impro

--- components/Cards/MovieCard.js
+++ components/Cards/MovieCard.js
@@ -1,19 +1,21 @@
 import Link from "next/link";
-import { useAmp } from "next/amp";
 import { useMoviePicture } from "../../hooks/useMoviePicture";
-
+import Image from "next/image";
 export default function MovieCard({ movie }) {
-  if ((movie.poster_path === null)) return <></>;
+  if (movie.poster_path === null) return <></>;
 
   return (
     <Link href={`/movie/${encodeURIComponent(movie.id)}`} key={movie.id}>
-      <div
-        id="movie-card"
-        style={{
-          backgroundImage: `url(${useMoviePicture(movie.poster_path, "w185")})`,
-        }}
-        className="block flex-shrink-0 rounded-md  mr-6 mb-6 w-44 h-64 bg-no-repeat cursor-pointer bg-cover border-b border-gray-800 hover:border-gray-200  "
-      ></div>
+      <div className="block flex-shrink-0  mr-6 mb-6 w-44 h-64  cursor-pointer   ">
+        <Image
+          src={useMoviePicture(movie.poster_path, "w185")}
+          height="145"
+          width="98"
+          layout="responsive"
+          alt="cast"
+          className="object-scale-down rounded-xl hover:opacity-50"
+        ></Image>
+      </div>
     </Link>
   );
 }
